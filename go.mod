2681e72c753ff8da478bc69bf64f119924810745
RandaeGS <garciasanchezrd@gmail.com>
2026-02-02 11:40:46 -0400

    feat: running timer

--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module github.com/randaegs/pomodogo
 go 1.25.6
 
 require (
+	github.com/charmbracelet/bubbles v0.21.0
 	github.com/charmbracelet/bubbletea v1.3.10
 	github.com/charmbracelet/lipgloss v1.1.0
+	github.com/common-nighthawk/go-figure v0.0.0-20210622060536-734e95fb86be
 )
 
 require (
@@ -13,7 +15,6 @@ require (
 	github.com/charmbracelet/x/ansi v0.10.1 // indirect
 	github.com/charmbracelet/x/cellbuf v0.0.13-0.20250311204145-2c3ea96c31dd // indirect
 	github.com/charmbracelet/x/term v0.2.1 // indirect
-	github.com/common-nighthawk/go-figure v0.0.0-20210622060536-734e95fb86be // indirect
 	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"log"
 	"os/exec"
+	"time"
 
+	"github.com/charmbracelet/bubbles/timer"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/common-nighthawk/go-figure"
@@ -14,19 +16,35 @@ type model struct {
 	shortRestTime int
 	longeRestTime int
 	workTime      int
+	remainingTime int
+	timer         timer.Model
 	width         int
 	height        int
 }
 
 func (m model) Init() tea.Cmd {
-	return nil
+	return m.timer.Stop()
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
+	case timer.TickMsg:
+		var cmd tea.Cmd
+		m.timer, cmd = m.timer.Update(msg)
+		return m, cmd
+
 	case tea.WindowSizeMsg:
 		m.width = msg.Width
 		m.height = msg.Height
+
+	case timer.StartStopMsg:
+		var cmd tea.Cmd
+		m.timer, cmd = m.timer.Update(msg)
+		return m, cmd
+
+	case timer.TimeoutMsg:
+		exec.Command("notify-send", "Finished").Run()
+
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "ctrl+c":
@@ -37,6 +55,11 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 				log.Fatal(err)
 			}
 			return m, nil
+		case "enter":
+			log.Println(m.timer.Running())
+			m.isRunning = !m.isRunning
+			return m, m.timer.Toggle()
+
 		}
 	}
 	return m, nil
@@ -49,10 +72,16 @@ func (m model) View() string {
 		lipgloss.Center,
 		lipgloss.Center,
 		lipgloss.JoinVertical(
-			lipgloss.Left,
+			lipgloss.Center,
 			Title(),
+			m.Timer(),
 		),
 	)
+}
+
+func (m model) Timer() string {
+	timer := figure.NewFigure(m.timer.View(), "", true).String()
+	return lipgloss.NewStyle().AlignVertical(lipgloss.Center).Render(timer)
 }
 
 func Title() string {
@@ -61,13 +90,22 @@ func Title() string {
 }
 
 func main() {
+	m := model{
+		isRunning:     false,
+		shortRestTime: 0,
+		longeRestTime: 0,
+		workTime:      0,
+		remainingTime: 0,
+		timer:         timer.NewWithInterval(time.Second*5, time.Second),
+	}
+
 	f, err := tea.LogToFile("debug.log", "debug")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer f.Close()
 
-	p := tea.NewProgram(model{}, tea.WithAltScreen())
+	p := tea.NewProgram(m, tea.WithAltScreen())
 
 	if _, err := p.Run(); err != nil {
 		log.Fatal(err)
